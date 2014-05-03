5affc2e545ee0ffb2a409127b0a75ebde29d96a1
jasminl <jasminl@jasper.co57.lan>
2014-05-02 20:13:45 -0400

    Removed path in royalroad app

--- src/apps/royalroadapp/main.cpp
+++ src/apps/royalroadapp/main.cpp
@@ -1,12 +1,19 @@
 #include "evo/Population.h"
 #include "evo/constants.h"
 #include "evo/Logger.h"
+#include <stdexcept>
 
 using namespace std;
 using namespace evo;
 
+/**
+ * Expects two arguments: a folder and a filename in which to store the logger's results
+ */
 int main(int argc, char *argv[])
 {
+	if(argc < 3)
+		throw runtime_error("royalroadapp::main: must specify a folder and file in separate arguments");
+
 	int population_size = 2 * 2 * 256;
 	int rr_fct = Population::FLOATING_RR;	//The type of royal road function
 	int nb_building_blocks = 16;
@@ -16,7 +23,7 @@ int main(int argc, char *argv[])
 	float crossover_rate = 0.7;
 	int bldblcksize = 2 * 8;
 
-	Logger log("/home/jasminl/workspace/cppmix/", "data");
+	Logger log(argv[1], argv[2]);
 
 	Population pop(population_size, nb_building_blocks, nb_neutral,
 			mutation_rate, crossover_rate, bldblcksize, rr_fct);
